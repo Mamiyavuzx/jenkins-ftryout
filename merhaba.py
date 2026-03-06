c6276cdb3af1caf2d98f493b0a452ac9bf2f70b1
Mamiyavuzx <Muhammed_Enes_Yavuz@hotmail.com>
2026-03-06 09:33:05 +0300

    Update merhaba.py

--- merhaba.py
+++ merhaba.py
@@ -1,12 +1,12 @@
-# Basit bir toplama deneyi
 def toplama(a, b):
     return a + b
 
 sonuc = toplama(10, 20)
 print(f"Deney Sonucu: {sonuc}")
 
-# Eger sonuc bekledigimiz gibi degilse sistem hata versin
-if sonuc != 30:
-    exit(1) # Hata kodu
+# BİLEREK HATA YAPIYORUZ: 10+20 asla 35 etmez!
+if sonuc != 35: 
+    print("HATA: Matematiksel tutarsizlik tespit edildi!")
+    exit(1) # Jenkins bu '1' kodunu gördüğü an build'i durdurur.
 else:
-    exit(0) # Basari kodu
+    exit(0)
