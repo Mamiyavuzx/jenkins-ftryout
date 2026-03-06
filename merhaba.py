6ed18071c5ec1106b7c79df228c7aec07d2b5319
Mamiyavuzx <Muhammed_Enes_Yavuz@hotmail.com>
2026-03-06 09:29:41 +0300

    Update merhaba.py

--- merhaba.py
+++ merhaba.py
@@ -1,1 +1,12 @@
-print("Merhaba Muhammed! Jenkins bu Python kodunu basariyla calistirdi.")
+# Basit bir toplama deneyi
+def toplama(a, b):
+    return a + b
+
+sonuc = toplama(10, 20)
+print(f"Deney Sonucu: {sonuc}")
+
+# Eger sonuc bekledigimiz gibi degilse sistem hata versin
+if sonuc != 30:
+    exit(1) # Hata kodu
+else:
+    exit(0) # Basari kodu
